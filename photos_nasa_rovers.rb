f8fb03d06e6b9b41f7dfd1b8d817b36b16c5d931
Alejandr Serrano <marialesr.17@gmail.com>
2021-02-06 17:00:07 -0600

    Avance uno

--- photos_nasa_rovers.rb
+++ photos_nasa_rovers.rb
@@ -4,26 +4,25 @@ require "json"
 
 def request (url, token = nil)
     url = URI("#{url}&api_key=#{token}")
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
     request = Net::HTTP::Get.new(url)
-    response = https.request(request)
+    response = http.request(request)
     return JSON.parse(response.read_body)
 end
 
- nasa_hash = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1", "sADGbmlSFHZbnl6FmA2skA94XsDrcigc1h3AqLSu")
- puts nasa_hash["photos"].length
 
+def buid_web_page(photo_hash) 
 
-def build_web_page (info_hash)
-    pictures = info_hash["photos"]
-    pictures_url = photos.map{|i| i["img_src"]} 
-    pictures_url.each do |photo|
-        photo.puts "<img src=#{photo}>"
-    end
-end
+   File.open('index.html', 'w') do |file|
+    puts photo_hash["photos"].to_s
+    file.puts "<img src='#{photo_hash["photos"][0]['img_src']}'>"
+   end
+ end   
 
+nasa_hash = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1", "sADGbmlSFHZbnl6FmA2skA94XsDrcigc1h3AqLSu")
 
+puts buid_web_page(nasa_hash)
 
 
 
