b5e6d47d0f474514aafe6c82c5e416b284f9fb7f
Alejandr Serrano <marialesr.17@gmail.com>
2021-02-08 09:59:07 -0600

    tercera parte lista

--- photos_nasa_rovers.rb
+++ photos_nasa_rovers.rb
@@ -11,15 +11,15 @@ def request (url, token = nil)
     return JSON.parse(response.read_body)
 end
 
-nasa_hash = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10", "sADGbmlSFHZbnl6FmA2skA94XsDrcigc1h3AqLSu")
-picture_web = nasa_hash["photos"].map do |i|
+nasa_api = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1", "sADGbmlSFHZbnl6FmA2skA94XsDrcigc1h3AqLSu")
+picture_web = nasa_api["photos"].map do |i|
     i["img_src"] 
 end
- 
+
 def build_page_nasa(info_hash) 
     File.open("index.html", "w") do |file| 
         file.puts"<html>\n<head>\n<title>API Nasa</title>\n</head>"
-        file.puts"<body>\n<h1 align = 'center'>Prueba photos Rovers</h1>\n<ul>"
+        file.puts"<body>\n<h1 align = 'center'>Photos Rovers Nasa</h1>\n<ul>"
         info_hash.each  do |photo|
             file.puts "<img src='#{photo}'width ='300' height = '200'>"    
         end
@@ -29,7 +29,21 @@ end
 
 puts build_page_nasa(picture_web)
 
-
-
-def photos_count 
+def photos_count(info_hash)
+    counter = Hash.new(0)
+    info_hash["photos"].each do |name_camera|
+        name_camera["camera"].each do |k, v|
+            if k == "name"
+                puts v
+            elsif counter.include? v
+                counter[v] += 1
+            else
+                counter[v] = 1
+            end
+        end
+    end
+    #puts "Name of camera and number of photos#{counter}"
 end
+#photos_count(nasa_api)
+
+ 
